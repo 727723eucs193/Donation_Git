e57dfe938e0d54981e5361245ad37a264c61164b
Ruby <727723eucs193@skcet.ac.in>
2025-08-04 10:34:49 +0530

    file added

---
+++ BACKEND/donation_git/src/main/java/com/example/donation_git/DonationGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.donation_git;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonationGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonationGitApplication.class, args);
+	}
+
+}
---
+++ BACKEND/donation_git/src/test/java/com/example/donation_git/DonationGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.donation_git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonationGitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
